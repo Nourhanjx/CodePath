661cf5262d97c510d06ecff448ee7ec1e201bfca
nourhanjx <nourhanjamalx@gmail.com>
2021-03-22 11:57:48 +0200

    First commit

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func valueChanged(_ sender: UISegmentedControl) {
+        
+        guard let amount = self.billAmountTextField.text else {return}
+        
+        let bill = Double(amount)
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = tipPercentages[sender.selectedSegmentIndex] * (bill ?? 0.0)
+        
+        let total = (bill ?? 0.0) + tip
+        
+        self.totalLbl.text = "$\(String(format: "%.2f", total))"
+        self.tipAmountLabel.text = String(format: "$%.2f", tip)
+    }
 }
 
